2136ec5e4824d2acec990fe7fba07120ce00f2b8
GoldFrosch <goldfrosch@daum.net>
2024-04-21 10:25:32 +0900

    feat: 내리는 눈 애니메이션 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { useEffect, useRef } from "react";
 import "./App.css";
+import { Snow } from "./snow";
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
+  useEffect(() => {
+    if (canvasRef.current) {
+      const snowParticle = new Snow({
+        canvas: canvasRef.current.getContext("2d"),
+        width: canvasRef.current.width,
+        height: canvasRef.current.height,
+      });
+
+      snowParticle.executeLoop();
+    }
+  }, []);
 
   return <canvas width={800} height={800} ref={canvasRef} />;
 }
---
+++ src/snow.ts
@@ -0,0 +1,111 @@
+interface particle {
+  x: number;
+  y: number;
+  radius: number;
+  color: string;
+  speed: number;
+}
+
+export class Snow {
+  private tick = 0;
+  private maxParticle = 100;
+  private width: number = 0;
+  private height: number = 0;
+  private particles: particle[] = [];
+
+  private canvas: CanvasRenderingContext2D | null;
+
+  private newLoop = this.loop.bind(this);
+
+  constructor({
+    canvas,
+    maxParticle = 100,
+    width,
+    height,
+  }: {
+    canvas: CanvasRenderingContext2D | null;
+    maxParticle?: number;
+    width: number;
+    height: number;
+  }) {
+    this.canvas = canvas;
+    this.maxParticle = maxParticle;
+    this.width = width;
+    this.height = height;
+  }
+
+  executeLoop() {
+    window.requestAnimationFrame(this.newLoop);
+  }
+
+  private loop() {
+    window.requestAnimationFrame(this.newLoop);
+    this.createParticle();
+    this.updateParticle();
+    this.resetParticle();
+    this.drawParticle();
+  }
+
+  private createParticle() {
+    if (this.tick % 10 === 0) {
+      if (this.particles.length < this.maxParticle) {
+        this.particles.push({
+          x: Math.random() * 1000,
+          y: 0,
+          radius: Math.random() * 4 + 1,
+          color: "white",
+          speed: Math.random() * 1 + 1,
+        });
+      }
+    }
+    if (this.tick === 600) {
+      this.tick = 0;
+    } else {
+      this.tick++;
+    }
+  }
+
+  private updateParticle() {
+    for (const i in this.particles) {
+      const part = this.particles[i];
+      part.y += part.speed;
+    }
+  }
+
+  private resetParticle() {
+    for (const i in this.particles) {
+      const part = this.particles[i];
+      if (part.y > this.height) {
+        this.particles[i] = {
+          x: Math.random() * 1000,
+          y: 0,
+          radius: Math.random() * 4 + 1,
+          color: "white",
+          speed: Math.random() * 1 + 1,
+        };
+      }
+    }
+  }
+
+  private drawParticle() {
+    const canvas = this.canvas;
+
+    if (canvas) {
+      canvas.fillStyle = "#040309";
+      canvas.fillRect(0, 0, this.width, this.height);
+
+      for (const i in this.particles) {
+        const particle = this.particles[i];
+
+        canvas.beginPath();
+        canvas.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2);
+        canvas.closePath();
+
+        canvas.fillStyle = particle.color;
+        canvas.globalAlpha = 1;
+
+        canvas.fill();
+      }
+    }
+  }
+}
